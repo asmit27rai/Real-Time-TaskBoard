8f6479c283795106706a47b290778e0ac3cc840e
asmb123 <asmit.barua.phe23@itbhu.ac.in>
2025-04-09 21:08:57 +0530

    socket.io client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,44 @@
+import { useEffect, useState, useRef } from "react";
+import { io, Socket } from "socket.io-client";
+
+type CustomData = {
+  id: string,
+  title: string,
+  description: string,
+  completed: boolean,
+}
+
 function App() {
+
+  const socketRef = useRef<Socket | null>(null);
+
+  useEffect(() => {
+    const socket = io('http://localhost:3000');
+    socketRef.current = socket;
+
+    socket.on('connect', () => {
+      console.log("ws connected");
+      socket.on('data', ()=>{
+        console.log(data);
+      })
+    });
+
+    return () => {
+      socket.disconnect();
+      console.log("ws disconnected");
+    };
+
+  }, [])
+
+  const [data, setData] = useState<CustomData[] | null>(null);
 
   return (
     <>
-      hi
+      <div className="w-full h-full text-black flex flex-col">
+        <div>
+
+        </div>
+      </div>
     </>
   )
 }
